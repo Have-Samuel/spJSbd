1d2472a7512baf4590d38cd59228685131de7bbc
Have Samuel <samiehave9@gmail.com>
2023-01-27 02:42:17 +0300

    Using the if statements

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-if (1 !== 1) {
-  console.log("It's true!")
+// if (1 !== 1) {
+//   console.log("It's true!")
+// }
+
+// // Exe->2
+// let ratiing = 2;
+// if (ratiing === 2) {
+//   console.log('You are a super')
+// }
+
+// // Exe3
+// let num = 35;
+
+// if (num % 2 !== 0) {
+//   console.log('ODD NUMBER!')
+// }
+
+// Performance Review
+// 3 - superstar
+// 2 - meets expectations
+// 1 - needs improvement
+// anything else - wtf?
+
+let rating = 3;
+if (rating === 3) {
+  console.log('You are a superstar!')
 }
 
-// Exe->2
-let ratiing = 2;
-if (ratiing === 2) {
-  console.log('You are a super')
-}
-
-// Exe3
-let num = 35;
-
-if (num % 2 !== 0) {
-  console.log('ODD NUMBER!')
+if (rating >= 3) { // this is not right
+  console.log('meets epectation!')
 }
