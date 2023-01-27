bc05e943777ec018c62cc52ce958a8b437ab4b2b
Have Samuel <samiehave9@gmail.com>
2023-01-28 00:09:13 +0300

    using PUSH and POP array methods

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let shopping = ['hut', 'books', 'mask', 'handkerchief'];
+// let shopping = ['hut', 'books', 'mask', 'handkerchief'];
 
-shopping[2] = 'knife'
+// shopping[2] = 'knife'
 
-shopping[shopping.length]  = 'dounat'
+// shopping[shopping.length]  = 'dounat'
+
+let games = ['football', 'rugby', 'netball', 'handball', 'cricket', 'basketball'];
+games.push('badminton');
+
+games.pop();
+
+const rejectedGame = games.pop();
