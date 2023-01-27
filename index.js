f7439fac561457a913cf47e74e384f27cb67e124
Have Samuel <samiehave9@gmail.com>
2023-01-28 00:45:29 +0300

    Using includes method

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ let int = [90, 97, 91, 94, 50]
 const allTogether = nums.concat(floats, int)
 
 console.log(allTogether);
+
+let fruits = ['paw paw', 'pinapple', 'guava', 'jack-fruit', 'orange']
+const allFruits = fruits.includes('orange')
+console.log(allFruits)
+
+if (fruits.includes('paw paw')) {
+  console.log('I cant have that for lunch!')
+}
