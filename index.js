728966c1b46b8511d7542ef5f3e5aa5cd04606ee
Have Samuel <samiehave9@gmail.com>
2023-01-28 19:23:55 +0300

    Arrays and Objects into each other

--- index.js
+++ index.js
@@ -1,119 +1,201 @@
-// earliest methods to add elements to the array
-let shopping = ['hut', 'books', 'mask', 'handkerchief'];
+// // earliest methods to add elements to the array
+// let shopping = ['hut', 'books', 'mask', 'handkerchief'];
 
-shopping[2] = 'knife'
+// shopping[2] = 'knife'
 
-shopping[shopping.length]  = 'dounat'
-// push() and pop()
-let games = ['football', 'rugby', 'netball', 'handball', 'cricket', 'basketball'];
-games.push('badminton');
+// shopping[shopping.length]  = 'dounat'
+// // push() and pop()
+// let games = ['football', 'rugby', 'netball', 'handball', 'cricket', 'basketball'];
+// games.push('badminton');
 
-games.pop();
+// games.pop();
 
-const rejectedGame = games.pop();
-// unshift and shift
-let animals = ['cow', 'goat', 'pig', 'hen', 'cat']
-animals.unshift('elephant')
+// const rejectedGame = games.pop();
+// // unshift and shift
+// let animals = ['cow', 'goat', 'pig', 'hen', 'cat']
+// animals.unshift('elephant')
 
-animals.shift()
+// animals.shift()
 
-let nums = [12, 23, 13, 34]
-let floats = [19, 39, 29, 81, 98];
-let int = [90, 97, 91, 94, 50]
-// concat()
-const allTogether = nums.concat(floats, int)
+// let nums = [12, 23, 13, 34]
+// let floats = [19, 39, 29, 81, 98];
+// let int = [90, 97, 91, 94, 50]
+// // concat()
+// const allTogether = nums.concat(floats, int)
 
-console.log(allTogether);
+// console.log(allTogether);
 
-let fruits = ['paw paw', 'pinapple', 'guava', 'jack-fruit', 'orange']
-const allFruits = fruits.includes('orange')
-console.log(allFruits)
-// includes
-if (fruits.includes('paw paw')) {
-  console.log('I cant have that for lunch!')
+// let fruits = ['paw paw', 'pinapple', 'guava', 'jack-fruit', 'orange']
+// const allFruits = fruits.includes('orange')
+// console.log(allFruits)
+// // includes
+// if (fruits.includes('paw paw')) {
+//   console.log('I cant have that for lunch!')
+// }
+// // indexOf
+// const selectedFruit = fruits.indexOf('guava')
+// console.log(selectedFruit);
+
+// // revered()
+// let alphas = ['K', 'A', 'B', 'A', 'R', 'E', 'E', 'B', 'E']
+// const reversedData = alphas.reverse()
+// console.log(reversedData)
+
+// // join()
+// const joinedData = alphas.reverse().join()
+// console.log(joinedData)
+
+// //slice
+// let animalKingdom = ['shark', 'whale', 'dolphine', 'bear', 'tortoise', 'lizard']
+// let swimmers = animalKingdom.slice(0, 3)
+// console.log(swimmers)
+// let aniGround = animalKingdom.slice(3)
+// console.log(aniGround)
+// let midAni = animalKingdom.slice(2, 5)
+// console.log(midAni)
+// console.log(animalKingdom)
+
+// //splice
+// let wild = ['cow', 'goat', 'pig', 'hen', 'cat', 'lion', 'snake'];
+// const newOne = wild.splice(1, 0, 'pig')
+// console.log(newOne)
+// const secondOne = wild.splice(2, 2)
+// console.log(secondOne)
+// const thirdOne = wild.splice(3, 0, 'BOOKS')
+// console.log(thirdOne)
+
+// // sort()
+// let names = ['yamu', 'nuru', 'mula', 'gula', 'zaka', 'angel', 'boboka']
+// const sortedData = names.sort()
+// console.log(sortedData)
+
+// // nested arrays
+// const maleFamale = [
+//   ['doe', 'back'],
+//   ['ewe', 'ram'],
+//   ['peahen', 'peacock']
+// ]
+
+// const select = maleFamale[1][1] = 'stag';
+// console.log(maleFamale)
+
+// // Arrays Exercise
+// const people = ['Greg', 'Mary', 'Devon', 'James'];
+// // removes elements from the front of the array
+// const remover = people.shift();
+// console.log(remover);
+// console.log(people);
+
+// // removes elements from the end
+// const secondRemover = people.pop()
+// console.log(secondRemover);
+// console.log(people);
+
+// // adds elements at the front of the array
+// const addData = people.unshift('Matt');
+// console.log(people);
+
+// // Adding element to end of the array
+// people.push('Have');
+// console.log(people);
+
+// // copying the array, with excludes => slice
+// const oneOfTheOther = people.slice(1)
+// console.log(oneOfTheOther)
+
+// // shows index of
+// const index = people.indexOf('Mary')
+// console.log(index)
+// const index2 = people.indexOf('Foo');
+// console.log(index2);
+
+// // Using splice
+// const people2 = ['Greg', 'Mary', 'Devon', 'James'];
+// const getOn = people2.splice(2, 1, 'Elizabeth', 'Artie')
+// console.log(people2)
+
+
+// const withBob = people2.concat('Bob');
+// console.log(withBob)
+
+// OBJECTS
+const fitBitdData = {
+  totalSteps: 308727,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workOutsThisWeek: '5 of 7',
+  avgGoodSleep: '3:13'
 }
-// indexOf
-const selectedFruit = fruits.indexOf('guava')
-console.log(selectedFruit);
 
-// revered()
-let alphas = ['K', 'A', 'B', 'A', 'R', 'E', 'E', 'B', 'E']
-const reversedData = alphas.reverse()
-console.log(reversedData)
+const numbers = {
+  100: 'one hundred',
+  16: 'sixteen'
+}
 
-// join()
-const joinedData = alphas.reverse().join()
-console.log(joinedData)
+const find = numbers[100]
+console.log(find);
 
-//slice
-let animalKingdom = ['shark', 'whale', 'dolphine', 'bear', 'tortoise', 'lizard']
-let swimmers = animalKingdom.slice(0, 3)
-console.log(swimmers)
-let aniGround = animalKingdom.slice(3)
-console.log(aniGround)
-let midAni = animalKingdom.slice(2, 5)
-console.log(midAni)
-console.log(animalKingdom)
+const palette = {
+  red: '#eb4dd4d',
+  yellow: '#f9ca24',
+  blue: '#30336b'
+};
 
-//splice
-let wild = ['cow', 'goat', 'pig', 'hen', 'cat', 'lion', 'snake'];
-const newOne = wild.splice(1, 0, 'pig')
-console.log(newOne)
-const secondOne = wild.splice(2, 2)
-console.log(secondOne)
-const thirdOne = wild.splice(3, 0, 'BOOKS')
-console.log(thirdOne)
+let mysteryColor = 'blue';
+// Adding elements into an Object
+const reviews = {};
 
-// sort()
-let names = ['yamu', 'nuru', 'mula', 'gula', 'zaka', 'angel', 'boboka']
-const sortedData = names.sort()
-console.log(sortedData)
+reviews['queenBee34'] = 45;
+reviews['mrSmith45'] = 3.03;
+reviews.gyal71 = 90.23;
 
-// nested arrays
-const maleFamale = [
-  ['doe', 'back'],
-  ['ewe', 'ram'],
-  ['peahen', 'peacock']
-]
+// Nesting arrays into objects
 
-const select = maleFamale[1][1] = 'stag';
-console.log(maleFamale)
+const student = {
+  firstName: 'Have',
+  secondName: 'Peace',
+  strength: ['football', 'Atheletics'],
+  exams: {
+    midterm: 86,
+    final: 70
+  }
+};
+const avg = (student.exams.midterm + student.exams.final) / 2;
+console.log(avg);
 
-// Arrays Exercise
-const people = ['Greg', 'Mary', 'Devon', 'James'];
-// removes elements from the front of the array
-const remover = people.shift();
-console.log(remover);
-console.log(people);
+const index = student.strength[0]
+console.log(index);
 
-// removes elements from the end
-const secondRemover = people.pop()
-console.log(secondRemover);
-console.log(people);
+const shoppingCart = [
+  {
+    product: 'jenga Classic',
+    price: 6.88,
+    quantity: 1
+  },
+  {
+    product: 'Echo Dot',
+    price: 29.88,
+    quantity: 3
+  },
+  {
+    product: 'Fire Stick',
+    price: 39.99,
+    quantity: 2
+  },
+];
 
-// adds elements at the front of the array
-const addData = people.unshift('Matt');
-console.log(people);
-
-// Adding element to end of the array
-people.push('Have');
-console.log(people);
-
-// copying the array, with excludes => slice
-const oneOfTheOther = people.slice(1)
-console.log(oneOfTheOther)
-
-// shows index of
-const index = people.indexOf('Mary')
-console.log(index)
-const index2 = people.indexOf('Foo');
-console.log(index2);
-
-// Using splice
-const people2 = ['Greg', 'Mary', 'Devon', 'James'];
-const getOn = people2.splice(2, 1, 'Elizabeth', 'Artie')
-console.log(people2)
-
-
-const withBob = people2.concat('Bob');
-console.log(withBob)
+const game = {
+  player1: {
+    userName: 'Blue',
+    playingAs: 'X'
+  },
+  player2: {
+    userName: 'Muffins',
+    playingAs: 'o'
+  },
+  board: [
+    ['o', null, 'X'],
+    ['X', 'o', null],
+    [null, 'o', 'X']
+  ]
+};
