6480bccd08025a8c16763bf6d954ef1237238f23
Have Samuel <samiehave9@gmail.com>
2023-01-29 17:58:36 +0300

    Applying the While Loop

--- index.js
+++ index.js
@@ -527,12 +527,19 @@
 //   j++;
 // }
 
+
+// while (guess !== target) {
+//   console.log(`Target: ${target} Guess: ${guess}`);
+//   guess = Math.floor(Math.random() * 10);
+// }
+
 const target = Math.floor(Math.random() * 10)
 let guess = Math.floor(Math.random() * 10);
-while (guess !== target) {
+
+while (true) {
+  if (guess === target)break;
   console.log(`Target: ${target} Guess: ${guess}`);
   guess = Math.floor(Math.random() * 10);
 }
-
 console.log(`Target: ${target} Guess: ${guess}`);
 console.log('CONGRATS YOU WIN!!')
