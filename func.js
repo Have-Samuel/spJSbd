5f3576686db9e7f09464641951e9ce95ebc09e12
Have Samuel <samiehave9@gmail.com>
2023-01-30 02:42:33 +0300

    More on Return Values

--- func.js
+++ func.js
@@ -1,15 +1,15 @@
 // FunctionRollDice
-function rollDice() {
-  let roll = Math.floor(Math.random() * 6) + 1;
-  console.log(`Rolled: ${roll}`);
-}
+// function rollDice() {
+//   let roll = Math.floor(Math.random() * 6) + 1;
+//   console.log(`Rolled: ${roll}`);
+// }
 
-function throwDice(numRolls) {
-  for (i = 0; i < numRolls; i++) {
-    rollDice();
-  }
-}
-throwDice(6)
+// function throwDice(numRolls) {
+//   for (i = 0; i < numRolls; i++) {
+//     rollDice();
+//   }
+// }
+// throwDice(6)
 
 // function greet() {
 //   console.log('Hi')
@@ -40,10 +40,33 @@ function square(x) {
   console.log() // this never works after return
 }
 
-function isPurple(color) {
-  if (color.toLowerCase() === 'purple') {
-    return true;
-  } else {
-    return false;
+// function isPurple(color) {
+//   if (color.toLowerCase() === 'purple') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// SHORTER WAY!
+// function isPurple(color) {
+//   if (color.toLowerCase() === 'purple') {
+//     return true;
+//   } 
+//     return false;
+// }
+  
+// MORE SHORTER WAY!
+// function isPurple(color) {
+//   return color.toLowerCase() === 'purple';
+// }
+
+function containPink(arr) {
+  for (let color of arr) {
+    if (color === 'pink' || color === 'magenta') {
+      return true;
+    }
   }
+  return false;
 }
+arr = ['red', 'magenta', 'pink']
