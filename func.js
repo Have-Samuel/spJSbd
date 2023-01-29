96b99c180c82b612b96d25aacc96cdbb41658e52
Have Samuel <samiehave9@gmail.com>
2023-01-30 01:50:31 +0300

    Introducing Arguments

--- func.js
+++ func.js
@@ -1,15 +1,20 @@
 // FunctionRollDice
 function rollDice() {
   let roll = Math.floor(Math.random() * 6) + 1;
-  console.log(roll);
+  console.log(`Rolled: ${roll}`);
 }
 
-function throwDice() {
-  rollDice();
-  rollDice();
-  rollDice();
-  rollDice();
-  rollDice();
-  rollDice();
-  rollDice();
+function throwDice(numRolls) {
+  for (i = 0; i < numRolls; i++) {
+    rollDice();
+  }
+}
+throwDice(6)
+
+// function greet() {
+//   console.log('Hi')
+// }
+
+function greet(nickname) {
+  console.log(`Hey ${nickname}`);
 }
