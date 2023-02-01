877ec8c39b8e11e0566684a300f85c5a3a9f5bcd
Have Samuel <samiehave9@gmail.com>
2023-02-01 17:28:27 +0300

    Trial5

--- trial.js
+++ trial.js
@@ -8,15 +8,20 @@
 // }
 // product();
 
-function printDay(num) {
-  const stream = {
-    1: 'sunday',
-    2: 'monday',
-    3: 'tuesday',
-    4: 'wedensday',
-    5: 'thrusday',
-    6: 'friday',
-    7: 'saturday'
-  }
-  return stream[num];
+// function printDay(num) {
+//   const stream = {
+//     1: 'sunday',
+//     2: 'monday',
+//     3: 'tuesday',
+//     4: 'wedensday',
+//     5: 'thrusday',
+//     6: 'friday',
+//     7: 'saturday'
+//   }
+//   return stream[num];
+// }
+
+
+function lastElement(arr) {
+  return arr[arr.length - 1];
 }
